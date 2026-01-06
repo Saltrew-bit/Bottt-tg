07b4393c24f4b95f2327da3f2918fa34dcb0084f
Saltrew-bit <ruoty363@gmail.com>
2026-01-06 13:51:14 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto
 
-BOT_TOKEN = "8219073859:AAH2qL0-w9mQTxGOFNqv-svRALHFQ8MDorw"
+BOT_TOKEN = "8229146472:AAHZHfHoR_ahWMYizdUI_ugOEp-ltD-bNpQ"
 ADMIN_ID = 1688416529
 CHANNEL_ID = "@AutoHub62Channel"
 
